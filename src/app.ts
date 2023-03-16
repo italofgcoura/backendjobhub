3f0f0eac83b4614352da2b63b06d2cbd77772a1f
Italo <italofabricio@outlook.pt>
2023-03-16 00:05:08 -0300

    Cors

--- src/app.ts
+++ src/app.ts
@@ -26,7 +26,7 @@ app.use(express.json());
 
 app.use((req, res, next) => {
 
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', '*');
   res.setHeader('Access-Control-Allow-Headers', ['Content-Type', 'authorization']);
 
